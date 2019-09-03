368cecc6b7390b6ab6eb282fd8311708eee459f3
unknown <lequangquochung1912@gmail.com>
2019-09-03 22:19:59 +0700

    .

--- Training01/Lesson22.cs
+++ Training01/Lesson22.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace Training01
 {
-    class Lesson22
+    public class Lesson22
     {
+        public static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Input a : ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine(result(a));
+
+        }
+        public static bool result(int n) { 
+            if(Math.Abs(n - 100) <= 20 || Math.Abs(n - 200) <= 20){
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 }
